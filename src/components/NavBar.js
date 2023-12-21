322da0c9693054d4e219a075040e19b6c7a75e86
johnlouis567 <ingram@mines.edu>
2023-12-20 20:33:15 -0700

    NavBar functional

---
+++ src/components/NavBar.js
@@ -0,0 +1,55 @@
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import logo from '../assets/img/logo.svg';
+import linkedInIcon from '../assets/img/linkedInIcon.svg';
+import githubIcon from '../assets/img/githubIcon.svg';
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const updateActiveLink = (link) => {
+    setActiveLink(link);
+  }
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+    <Container>
+      <Navbar.Brand href="#home">
+        <img alt="Logo" src={logo} height="75px" width="75px"/>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav">
+        <span className="navbar-toggler-icon"></span>
+      </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => updateActiveLink('home')}>Home</Nav.Link>
+          <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => updateActiveLink('skills')}>Skills</Nav.Link>
+          <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => updateActiveLink('projects')}>Projects</Nav.Link>
+        </Nav>
+        <span className="navbar-text">
+          <div className="social-icons">
+            <a href="https://www.linkedin.com/in/john-louis-ingram/" target="_blank"><img src={linkedInIcon} alt="LinkedIn" height="75px" width="75px"/></a>
+            <a href="https://github.com/johnlouis567" target="_blank"><img src={githubIcon} alt="GitHub" height="75px" width="75px" target="_blank"/></a>
+          </div>
+          <button className="wwd" onClick={() => console.log("Contact Me")}><span>Contact Me</span></button>
+        </span>
+      </Navbar.Collapse>
+    </Container>
+    </Navbar>
+  )
+}
