7ab327fbd7e80e297bbf1fa0b1b328b95da88dc4
johnlouis567 <ingram@mines.edu>
2023-12-22 15:28:51 -0700

    Skills Section Functional:

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './assets/img/logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar.js';
 import { Banner } from './components/Banner.js';
+import { Skills } from './components/Skills.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
