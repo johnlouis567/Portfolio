37c47b067d50495155168dac7f5fece9cd030721
johnlouis567 <ingram@mines.edu>
2024-02-08 13:59:09 -0700

    Removed footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { NavBar } from './components/NavBar.js';
 import { Banner } from './components/Banner.js';
 import { Skills } from './components/Skills.js';
 import { Projects } from './components/Projects.js';
-import { Footer } from './components/Footer.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      <Footer />
     </div>
   );
 }
